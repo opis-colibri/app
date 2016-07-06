986157945f528c7094c7370974a7a2d62b6f0827
Marius Sarca <marius.sarca@gmail.com>
2016-07-06 22:49:11 +0300

    Updated

--- public/index.php
+++ public/index.php
@@ -18,11 +18,6 @@
  * limitations under the License.
  * ============================================================================ */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('opcache.enable', 0);
-error_reporting(-1);
-
 $app = require_once __DIR__ . '/../bootstrap.php';
 
 $app->run();
